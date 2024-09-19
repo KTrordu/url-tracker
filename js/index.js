41ccc9e4db9cbef8fce7a247c940f68c512a7ff6
Alptuğ Yılmaz <alptug.yilmaz@outlook.com>
2024-09-19 17:34:53 +0300

    js update

--- js/index.js
+++ js/index.js
@@ -5,15 +5,27 @@ const inputEl = document.getElementById("input-el");
 
 const ulEl = document.getElementById("ul-el");
 
-let listItems = "";
-
 inputBtn.addEventListener("click", function() {
     
-    myLeads.push(inputEl.value)
+    myLeads.push(inputEl.value);
+
+    inputEl.value = "";
+
+    renderLeads();
+});
+
+function renderLeads() {
+    
+    let listItems = "";
     
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>";
+        listItems += `
+        <li> 
+            <a target = '_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>`;
     }
 
     ulEl.innerHTML = listItems;
-});
+}
