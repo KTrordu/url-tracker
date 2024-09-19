6ee8f54db137214ca9118120c8b6be52f260ba11
Alptuğ Yılmaz <alptug.yilmaz@outlook.com>
2024-09-19 18:11:12 +0300

    js update for variable name localizations

--- js/index.js
+++ js/index.js
@@ -1,28 +1,30 @@
 const inputBtn = document.getElementById("input-btn");
 
-let myLeads = [];
+let myURLs = [];
 const inputEl = document.getElementById("input-el");
 
 const ulEl = document.getElementById("ul-el");
 
+
+
 inputBtn.addEventListener("click", function() {
     
-    myLeads.push(inputEl.value);
+    myURLs.push(inputEl.value);
 
     inputEl.value = "";
 
-    renderLeads();
+    renderURLs();
 });
 
-function renderLeads() {
+function renderURLs() {
     
     let listItems = "";
     
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < myURLs.length; i++) {
         listItems += `
         <li> 
-            <a target = '_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
+            <a target = '_blank' href='${myURLs[i]}'>
+                ${myURLs[i]}
             </a>
         </li>`;
     }
