748de52f1eeb33d190ef35d764c3aaa8b82a8ca3
Alptuğ Yılmaz <alptug.yilmaz@outlook.com>
2024-09-19 21:59:10 +0300

    html and css update for save tab and delete all buttons, js update for new buttons' functionality, manifest.json update for permission from chrome.tabs api and new version release

--- js/index.js
+++ js/index.js
@@ -2,31 +2,59 @@ let myURLs = [];
 
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 const ulEl = document.getElementById("ul-el");
 
-let URLsFromLocalStorage = localStorage.getItem("myURLs");
-myURLs = JSON.parse(URLsFromLocalStorage);
+const URLsFromLocalStorage = JSON.parse(localStorage.getItem("myURLs"));
+
+if (URLsFromLocalStorage) {
+    myURLs = URLsFromLocalStorage;
+    render(myURLs);
+}
+
+tabBtn.addEventListener("click", function () {
+    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+        myURLs.push(tabs[0].url);
+        localStorage.setItem("myURLs", JSON.stringify(myURLs));
+        render(myURLs);
+
+    });
+
+});
 
 inputBtn.addEventListener("click", function() {
-    
+
     myURLs.push(inputEl.value);
     inputEl.value = "";
 
     localStorage.setItem("myURLs", JSON.stringify(myURLs));
 
-    renderURLs();
+    render(myURLs);
 });
 
-function renderURLs() {
+deleteBtn.addEventListener("dblclick", function() {
+
+    localStorage.clear();
+
+    myURLs = [];
+
+    render(myURLs);
+
+});
+
+function render(URLs) {
     
     let listItems = "";
     
-    for (let i = 0; i < myURLs.length; i++) {
+    for (let i = 0; i < URLs.length; i++) {
         listItems += `
         <li> 
-            <a target = '_blank' href='${myURLs[i]}'>
-                ${myURLs[i]}
+            <a target = '_blank' href='${URLs[i]}'>
+                ${URLs[i]}
             </a>
         </li>`;
     }
