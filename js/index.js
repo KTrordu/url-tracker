58381a2760c4ecd55ad947bcb3648a888c32063a
Alptuğ Yılmaz <alptug.yilmaz@outlook.com>
2024-09-19 18:38:23 +0300

    js update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
-const inputBtn = document.getElementById("input-btn");
-
 let myURLs = [];
+
 const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
 
 const ulEl = document.getElementById("ul-el");
 
-
+let URLsFromLocalStorage = localStorage.getItem("myURLs");
+myURLs = JSON.parse(URLsFromLocalStorage);
 
 inputBtn.addEventListener("click", function() {
     
     myURLs.push(inputEl.value);
-
     inputEl.value = "";
+
+    localStorage.setItem("myURLs", JSON.stringify(myURLs));
 
     renderURLs();
 });
